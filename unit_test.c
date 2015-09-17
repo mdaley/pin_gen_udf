0d97ef65bc9acc6ee0df401a9d4aeeee34f20278
Matthew Daley <matt.daley@blueyonder.co.uk>
2015-09-17 17:30:41 +0100

    unit tests that generate pins.

--- unit_test.c
+++ unit_test.c
@@ -36,6 +36,17 @@ static void set_arg_as_string(UDF_ARGS* args, int index, char* value) {
   snprintf(args->args[index], 255, "%s", value);
 }
 
+static int is_valid_pin(char* value, int length) {
+  int i;
+  for (i = 0; i < length; i++) {
+    char v = value[i];
+    if (!((v >= 'A' && v <= 'Z') || (v >= '0' && v <= '9')))
+      return -1;
+  }
+
+  return 0;
+}
+
 int tests_run = 0;
 
 static char* init_returns_error_when_no_parameters_supplied() {
@@ -132,6 +143,60 @@ static char* init_returns_success_when_valid_parameter_is_supplied() {
     return "incorrect result code received.";
 }
 
+static char* deinit_succeeds() {
+  pin_gen_udf_deinit(NULL);
+  return NULL;
+}
+
+static char* pin_generation_succeeds() {
+  UDF_ARGS* udf_args = alloc_udf_args(1);
+  set_arg_as_int(udf_args, 0, 16);
+
+  char result[256];
+  unsigned long result_length;
+  char is_null;
+  char error;
+
+  char* pin = pin_gen_udf(NULL, udf_args, result, &result_length, &is_null, &error);
+
+  free(udf_args);
+
+  if (result_length == 16 && is_null == 0 && error == 0 && is_valid_pin(result, result_length) == 0)
+    return NULL;
+  else
+    return "Incorrect response from pin generation";
+}
+
+static char* generated_pins_are_different() {
+  UDF_ARGS* udf_args = alloc_udf_args(1);
+  set_arg_as_int(udf_args, 0, 16);
+
+  int iterations = 1000;
+  char result[iterations][256];
+  unsigned long result_length;
+  char is_null;
+  char error;
+
+  int i, j;
+  for(i = 0; i < iterations; i++) {
+    char* pin = pin_gen_udf(NULL, udf_args, result[i], &result_length, &is_null, &error);
+  }
+
+  free(udf_args);
+
+  // OK, using some sort of hashmap might be better for these comparisons but this will do!
+  for(i = 0; i < iterations; i++) {
+    for(j = 0; j < iterations; j++) {
+      if (i != j) {
+        if (strncmp(result[i], result[j], 16) == 0)
+          return "Two pins were identical, which is not good at all!";
+      }
+    }
+  }
+
+  return NULL;
+}
+
 static char* all_tests() {
   mu_run_test(init_returns_error_when_no_parameters_supplied);
   mu_run_test(init_returns_error_when_more_than_one_parameter_supplied);
@@ -139,6 +204,11 @@ static char* all_tests() {
   mu_run_test(init_returns_error_when_one_integer_parameter_supplied_but_it_is_too_small);
   mu_run_test(init_returns_error_when_one_integer_parameter_supplied_but_it_is_too_large);
   mu_run_test(init_returns_success_when_valid_parameter_is_supplied);
+
+  mu_run_test(deinit_succeeds);
+
+  mu_run_test(pin_generation_succeeds);
+  mu_run_test(generated_pins_are_different);
   return 0;
 }
 
